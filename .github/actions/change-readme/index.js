db8ad6051147195e97b3877fa0fc9973e56206fe
Rafa Ferrer <rafaelferreralcaraz@gmail.com>
2023-01-27 18:05:02 +0100

    Test commit

---
+++ .github/actions/change-readme/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const {meme} = require('meme-maker');
+const core = require('@actions/core');
+
+const result = core.getInput('resultado-tests');
+const imagePath = (result === 'success' ? '.github/actions/change-readme/img/good.jpg' : '.github/actions/change-readme/img/bad.jpg');
+const outPutName = (Math.random() + 1).toString(36).substring(7);
+const topText = test_results === 'success' ? core.getInput('frase_positiva').toUpperCase() : core.getInput('frase_negativa').toUpperCase();
+const bottomText = 'Y LO SABES';
+
+
+let memeMaker = require('meme-maker')
+
+let options = {
+  image: imagePath,
+  outfile: '.github/actions/change-redame/outputImg/' + outPutName,
+  topText: topText,
+  bottomText: bottomText,
+  fontSize: 30,
+  fontFill: '#FFFFFF'
+}
+
+memeMaker(options, function(err) {
+  if(e) throw new Error(err)
+  console.log('Image saved: ' + options.outfile);
+  fs.appendFileSync('README.md',  `![alt text](https://github.com/RFerrerIesEstacio/actividadGitHubActions/blob/readme-job/.github/actions/change-readme/outputImage/${outPutName}?raw=true)\n`);
+  core.setOutput('exit-status', 'Meme añadido al readme');
+});
