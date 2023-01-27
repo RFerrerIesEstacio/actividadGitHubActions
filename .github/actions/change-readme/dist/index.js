1fb0d7fc1525179692fcc417f8d0b4ff163284f9
Rafa Ferrer <rafaelferreralcaraz@gmail.com>
2023-01-27 22:08:15 +0100

    Test 14545554

--- .github/actions/change-readme/dist/index.js
+++ .github/actions/change-readme/dist/index.js
@@ -8300,7 +8300,7 @@ const memeMaker = __nccwpck_require__(4989)
 
 const result = core.getInput('resultado-tests');
 const imagePath = (result === 'success' ? '.github/actions/change-readme/img/good.jpg' : '.github/actions/change-readme/img/bad.jpg');
-const outPutName = (Math.random() + 1).toString(36).substring(7);
+const outPutName = (Math.random() + 1).toString(36).substring(7) + '.jpg';
 const topText = result === 'success' ? core.getInput('frase_positiva').toUpperCase() : core.getInput('frase_negativa').toUpperCase();
 const bottomText = 'Y LO SABES';
 
@@ -8318,7 +8318,7 @@ let options = {
 memeMaker(options, function(err) {
   if(err) throw new Error(err)
   console.log('Image saved: ' + options.outfile);
-  fs.appendFileSync('README.md',  `![alt text](https://github.com/RFerrerIesEstacio/actividadGitHubActions/blob/readme-job/.github/actions/change-readme/outputImage/${outPutName}?raw=true)\n`);
+  fs.appendFileSync('README.md', `![alt text](https://github.com/RFerrerIesEstacio/actividadGitHubActions/blob/readme-job/.github/actions/change-readme/outputImg/${outPutName}?raw=true)\n`);
   core.setOutput('exit-status', 'Meme añadido al readme');
 });
 })();
